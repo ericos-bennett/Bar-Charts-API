ebf6bb3801fec861223d03079429486071519bd7
Eric <ericos.bennett@gmail.com>
2020-12-21 18:00:08 +0000

    starting html framework, js test inputs

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-$(document).ready(function () {
-  $('a').click(function (event) {
-    alert('As you can see, the link no longer took you to jquery.com');
+const drawBarChart = function (data, options, element) {
+  //building
+};
 
-    event.preventDefault();
-  });
-});
+// Without label names
+testData1 = [1, 2, 3];
+
+// With label names
+testData2 = [
+  [10, 'el1'],
+  [20, 'el2'],
+  [30, 'el3']
+];
+
+testOption = {
+  valuesPosition: 'top/centre/bottom',
+  // barColor and labelColor can be arrays for multiple colors
+  barColor: 'black/#000000/rgb(100,200,100)/rgba(123,123,123,0.4)',
+  labelColor: 'black/#000000/rgb(100,200,100)/rgba(123,123,123,0.4)',
+  // barSpacing effects bar width
+  barSpacing: '10px/10%',
+  title: 'My Chart',
+  titleSize: '16px/10%',
+  titleColor: 'black/#000000/rgb(100,200,100)/rgba(123,123,123)'
+};
